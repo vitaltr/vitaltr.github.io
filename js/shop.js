9e3d03ea2fd1da50a478ba060a8f97a370a5eccc
pcb tasarımları <34417478+vitaltr@users.noreply.github.com>
2026-05-21 16:03:41 +0300

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,29 +1,44 @@
-let data = [];
+let data = [
+  {
+    title: "Elektronik",
+    products: [
+      {
+        name: "PCB Kit",
+        basePrice: 450,
+        img: "https://picsum.photos/200"
+      },
+      {
+        name: "IoT Kit",
+        basePrice: 600,
+        img: "https://picsum.photos/201"
+      }
+    ]
+  },
+  {
+    title: "Yazılım",
+    products: [
+      {
+        name: "AI Bot",
+        basePrice: 900,
+        img: "https://picsum.photos/202"
+      }
+    ]
+  }
+];
+
 let cart = [];
 let active = 0;
-
-async function load(){
-  let res = await fetch("urunler.json");
-  data = await res.json();
-
-  renderMenu();
-  render();
-}
 
 function renderMenu(){
   let menu = document.getElementById("menu");
   menu.innerHTML = "";
 
-  data.forEach((p,i)=>{
-    menu.innerHTML += `
-      <button onclick="openPage(${i})">
-        ${p.title}
-      </button>
-    `;
+  data.forEach((c,i)=>{
+    menu.innerHTML += `<button onclick="openCat(${i})">${c.title}</button>`;
   });
 }
 
-function openPage(i){
+function openCat(i){
   active = i;
   render();
 }
@@ -32,33 +47,34 @@ function render(){
   let app = document.getElementById("app");
   app.innerHTML = "";
 
-  let page = data[active];
-
-  page.products.forEach(p=>{
+  data[active].products.forEach(p=>{
     app.innerHTML += `
       <div class="product">
         <img src="${p.img}" width="150">
-
         <h3>${p.name}</h3>
         <p>${p.basePrice} TL</p>
-
-        <input id="txt${p.ref}" placeholder="${p.inputHint}">
-
-        <button onclick='addCart("${p.name}",${p.basePrice})'>
-          Sepete Ekle
-        </button>
+        <button onclick="addCart('${p.name}',${p.basePrice})">Sepete Ekle</button>
       </div>
     `;
   });
 
-  updateCart();
+  document.getElementById("cartCount").innerText = cart.length;
 }
 
 function addCart(name,price){
   cart.push({name,price});
-  updateCart();
-}
-
-function updateCart(){
   document.getElementById("cartCount").innerText = cart.length;
 }
+
+function sendOrder(){
+  let msg = "Yeni Sipariş:%0A";
+
+  cart.forEach(c=>{
+    msg += `- ${c.name} ${c.price}TL%0A`;
+  });
+
+  window.open("https://wa.me/90XXXXXXXXXX?text="+msg);
+}
+
+renderMenu();
+render();
