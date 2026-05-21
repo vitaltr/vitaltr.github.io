21f6defea58619c5d5c67a136cc62030458cd53c
pcb tasarımları <34417478+vitaltr@users.noreply.github.com>
2026-05-21 16:23:20 +0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,39 +1,67 @@
 let data = JSON.parse(localStorage.getItem("data")) || [];
 
+/* LOGIN */
 function login(){
 if(pass.value=="1234"){
 panel.style.display="block";
+renderCats();
+renderList();
 }
 }
 
-function addProduct(){
+/* KATEGORİLER */
+function renderCats(){
+let cat=document.getElementById("cat");
+cat.innerHTML="";
 
-let page = data.find(x=>x.title==title.value);
-
-let newProduct = {
-name:name.value,
-price:parseInt(price.value),
-img:img.value
-};
-
-if(page){
-page.products.push(newProduct);
-}else{
-data.push({
-id:"p"+Date.now(),
-title:title.value,
-products:[newProduct]
+data.forEach((c,i)=>{
+cat.innerHTML+=`<option value="${i}">${c.title}</option>`;
 });
 }
 
+/* KATEGORİ EKLE */
+function addCategory(){
+data.push({
+title:title.value,
+products:[]
+});
+
 save();
+renderCats();
+renderList();
 }
 
-function deletePage(){
-data = data.filter(x=>x.id !== del.value);
+/* ÜRÜN EKLE */
+function addProduct(){
+let i=document.getElementById("cat").value;
+
+data[i].products.push({
+id:Date.now(),
+name:name.value,
+price:parseInt(price.value),
+img:img.value
+});
+
 save();
+renderList();
 }
 
+/* SİSTEM GÖRÜNTÜ */
+function renderList(){
+let list=document.getElementById("list");
+list.innerHTML="";
+
+data.forEach(c=>{
+list.innerHTML+=`
+<div class="box">
+<h3>${c.title}</h3>
+<p>${c.products.length} ürün</p>
+</div>
+`;
+});
+}
+
+/* KAYDET */
 function save(){
 localStorage.setItem("data",JSON.stringify(data));
 alert("Kaydedildi");
