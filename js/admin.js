af5f22db8bccb42139ebf97091238a69b2c57dd5
pcb tasarımları <34417478+vitaltr@users.noreply.github.com>
2026-05-21 16:04:32 +0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,25 @@
+let data = JSON.parse(localStorage.getItem("data")||"[]");
 
+function login(){
+  if(pass.value=="1234"){
+    panel.style.display="block";
+  }
+}
+
+function add(){
+  let obj = {
+    title:"Yeni Kategori",
+    products:[
+      {
+        name:name.value,
+        basePrice:price.value,
+        img:"https://picsum.photos/200"
+      }
+    ]
+  };
+
+  data.push(obj);
+  localStorage.setItem("data",JSON.stringify(data));
+
+  alert("eklendi");
+}
